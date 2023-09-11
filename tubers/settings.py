7a275ae76116178d7a4cb957124fa40f1c822e98
atul120212 <atulsharma28092004@gmail.com>
2023-09-11 15:27:59 +0530

    day3 completed

--- tubers/settings.py
+++ tubers/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tubers/static')
 ]
 # Default primary key field type
