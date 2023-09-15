cdc1f716016b2956e97af08d33f22e9579b5cef2
atul120212 <atulsharma28092004@gmail.com>
2023-09-15 23:24:21 +0530

    day7_completed

--- webpages/views.py
+++ webpages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from .models import Slider
+from .models import Slider, Team
+
+
+
 
 # Create your views here.
 def home(request):
     sliders = Slider.objects.all()
+    teams= Team.objects.all()
     data = {
         'sliders': sliders,
+        'teams': teams,
     }
     return render(request, 'webpages/home.html', data)
 def about(request):
