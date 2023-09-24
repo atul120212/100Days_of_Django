3c5b1cc565d20367f7b862ebce120a6020fe1095
atul120212 <atulsharma28092004@gmail.com>
2023-09-24 22:41:21 +0530

    day16_completed

--- youtubers/views.py
+++ youtubers/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Youtuber
 
 # Create your views here.
 def youtubers(request):
-    return render(request, 'youtubers/youtubers.html')
+    tubers = Youtuber.objects.order_by('-created_date')
+    data = {
+        'tubers': tubers,
+    }
+    return render(request, 'youtubers/youtubers.html', data)
+
 
 def youtubers_detail(request, id):
     pass
