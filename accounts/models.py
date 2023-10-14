667db33b5b82289fd4370ad6e7999651880b7119
atul120212 <atulsharma28092004@gmail.com>
2023-10-14 13:50:54 +0530

    day29-completed

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Receipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    receipe_name = models.CharField(max_length=200)
+    receipe_description = models.TextField()
+    receipe_image = models.ImageField(upload_to='receipe_images', blank=True)
